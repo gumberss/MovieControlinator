dbc99e3aa644de20ff88f0b0e388e0f8ea7e4865
Gumbers <gustavomarinsuppi@gmail.com>
2020-04-19 15:35:23 -0300

    feat: adding volume op and volume down on the client. Refac: adding styles and make the views and buttons beauty

--- movie-controlinator-client/App.tsx
+++ movie-controlinator-client/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import io from 'socket.io-client'
+import { StyleSheet, Text, View, Button, SafeAreaView } from 'react-native';
 
-const socket = io('http://192.168.100.77:5000');
+import io from 'socket.io-client'
+import Constants from 'expo-constants';
+
+const socket = io( `http://${location.hostname}:5000`);
+
 /*
 ', {
   transports: ['websocket'], jsonp: false
@@ -19,42 +22,83 @@ export default class App extends React.Component {
     socket.emit('play-pause')
   }
 
-  foward(type: string) {
+  forward(type: string) {
     socket.emit('jump-forward', type)
   }
 
   back(type: string) {
     socket.emit('jump-back', type)
   }
+  
+  volumeUp() {
+    socket.emit('volume-up')
+  }
+
+  volumeDown() {
+    socket.emit('volume-down')
+  }
 
   render() {
     return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.buttonContainer} >
+          <Text style={styles.title} >Contro your computer!!</Text>
+          <Button title="Play / Pause" onPress={this.playPause} />
+        </View>
 
-      <View style={styles.container}>
-        <Text>Contro your computer!!</Text>
-        <hr />
-        <button onClick={this.playPause}>
-          <Text>Play / Pause</Text>
-        </button>
-        <hr />
-        <button onClick={() => this.foward('three-seconds')}>
-          <Text>Jump 3 seconds</Text>
-        </button>
-        <hr />
-        <button onClick={() => this.back('three-seconds')}>
-          <Text>Back 3 seconds</Text>
-        </button>
-        <hr />
-      </View>
+
+        <View style={styles.groupButtonContainer} >
+          <View style={styles.groupButton}>
+            <Button title="Back 3 seconds" onPress={() => this.back('three-seconds')} />
+          </View>
+
+          <View style={styles.groupButton}>
+            <Button title="Jump 3 seconds" onPress={() => this.forward('three-seconds')} />
+          </View>
+
+        </View >
+
+        <View style={styles.groupButtonContainer}>
+          <View style={styles.groupButton}>
+            <Button title="Volume Down" onPress={this.volumeDown} />
+          </View>
+
+          <View style={styles.groupButton}>
+            <Button title="Volume Up" onPress={this.volumeUp} />
+          </View>
+        </View>
+
+      </SafeAreaView>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    display: 'flex',
+    marginTop: Constants.statusBarHeight,
+    marginHorizontal: 16,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'stretch',
+    justifyContent: 'flex-start'
   },
+  buttonContainer: {
+    marginBottom: '10px',
+  },
+  groupButton: {
+    width: '49%',
+    height: 40
+  },
+  groupButtonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderBottomColor: 'gray',
+    borderBottomWidth: 1,
+    marginBottom: '16px'
+
+  },
+  title: {
+    alignSelf: 'center'
+  }
 });
